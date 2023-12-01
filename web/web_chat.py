81dd588b669470e3f809cb508a8027f8c86b2519
julian nothing <julian.57@outlook.fr>
2023-12-01 22:27:29 +0100

    Add 2 buttons for download historic

--- web/web_chat.py
+++ web/web_chat.py
@@ -1,14 +1,14 @@
+import csv
+import os
 import requests
 import json 
 import gradio as gr
+from datetime import datetime
 
 
 def start_web_ui(model, theme=None):
     url = "http://localhost:11434/api/generate"
-    headers = {
-        'Content-Type': "application/json",
-    }
-
+    headers = {'Content-Type': "application/json",}
     conversation_history = []
 
     def generate_response(prompt, chat_history):
@@ -33,6 +33,47 @@ def start_web_ui(model, theme=None):
         else:
             return "Error: Unable to fetch response", chat_history
 
+    def download_html():
+        html_content = """
+        <html>
+        <head><title>Conversation History</title></head>
+        <body>
+            <h2>Conversation History</h2>
+            <table style="border-collapse: collapse; width: 100%;">
+        """
+        for i in range(0, len(conversation_history), 2):
+            user = conversation_history[i] if i < len(conversation_history) else ""
+            model = conversation_history[i + 1] if i + 1 < len(conversation_history) else ""
+            
+            html_content += f"""
+            <tr>
+                <td style="border: 1px solid black; padding: 8px;"><b>User:</b></td>
+                <td style="border: 1px solid black; padding: 8px;">{user}</td>
+            </tr>
+            <tr>
+                <td style="border: 1px solid black; padding: 8px;"><b>Model:</b></td>
+                <td style="border: 1px solid black; padding: 8px;">{model}</td>
+            </tr>
+            """
+        html_content += """
+            </table>
+        </body>
+        </html>
+        """
+        filename = f"conversation_history_{datetime.now().strftime('%d%m%Y_%H%M%S')}.html"
+        with open(os.path.join(os.path.expanduser('~'), 'Downloads', filename), 'w', encoding='utf-8') as file:
+            file.write(html_content)
+
+    def download_csv():
+        filename = f"chat_history_{datetime.now().strftime('%d%m%Y_%H%M%S')}.csv"
+        with open(os.path.join(os.path.expanduser('~'), 'Downloads', filename), 'w', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerow(["User", "Model"])
+            for index in range(0, len(conversation_history), 2):
+                user = conversation_history[index]
+                model = conversation_history[index + 1] if index + 1 < len(conversation_history) else ""
+                writer.writerow([user, model])
+                
     if theme == "soft":
         theme = gr.themes.Soft() 
     elif theme == "base":
@@ -50,6 +91,8 @@ def start_web_ui(model, theme=None):
             gradio-app {background-color: #212121 !important;}
             #component-3 {width: 30% !important; margin: 0 auto !important; font-size: 1.25em !important;}
             #component-4 {width: 30% !important; margin: 0 auto !important; font-size: 1.25em !important;}
+            #component-5 {width: 30% !important; margin: 0 auto !important; font-size: 1.25em !important;}
+            #component-6 {width: 30% !important; margin: 0 auto !important; font-size: 1.25em !important;}
           """
     
     with gr.Blocks(title="Agentis", theme=theme, css=css) as agentis:
@@ -58,7 +101,12 @@ def start_web_ui(model, theme=None):
         btn_submit = gr.Button(value="Submit", variant='primary')
         btn_submit.click(generate_response, [msg, chatbot], [msg, chatbot])
         clear = gr.ClearButton([msg, chatbot], variant='stop')
-
+        btn_download_html = gr.Button(value="HTML History")
+        btn_download_html.click(download_html)
+        btn_download_csv = gr.Button(value="CSV History")
+        btn_download_csv.click(download_csv)
+        
         msg.submit(generate_response, [msg, chatbot], [msg, chatbot])
         
         agentis.launch(favicon_path="web/static/agentis_favicon.ico", inbrowser=True)
+        
