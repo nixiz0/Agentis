3eba7c4c88b6989135d46bb01ff77b8dcd5ba6a6
julian nothing <julian.57@outlook.fr>
2024-01-08 16:09:50 +0100

    Add the possibility to talk with the local AI

---
+++ audio/audio_chat.py
@@ -0,0 +1,64 @@
+import pyttsx3
+import speech_recognition as sr
+import requests
+import json
+
+
+def start_talk_chatbot(model, language="en-EN", mic_index=0, voice_id='HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\MSTTS_V110_enGB_HazelM'):
+    url = "http://localhost:11434/api/generate"
+    headers = {'Content-Type': "application/json",}
+    conversation_history = []
+    
+    # Initialize the text-to-speech engine
+    engine = pyttsx3.init()
+        
+    # Set the selected voice
+    engine.setProperty('voice', voice_id)
+
+    # Initialize the voice recognizer
+    recognizer = sr.Recognizer()
+
+    def generate_response(prompt, chat_history):
+        conversation_history.append(prompt)
+        full_prompt = "\n".join(map(str, conversation_history))
+        
+        data = {
+            "model": model,
+            "stream": False,
+            "prompt": full_prompt,
+        }
+
+        response = requests.post(url, headers=headers, data=json.dumps(data))
+
+        if response.status_code == 200:
+            response_text = response.text
+            data = json.loads(response_text)
+            actual_response = data["response"]
+            chat_history.append((prompt, actual_response))
+
+            # Read response to user
+            engine.say(actual_response)
+            engine.runAndWait()
+
+            return "", chat_history
+        else:
+            return "Error: Unable to fetch response", chat_history
+    
+    while True:
+        with sr.Microphone(device_index=mic_index) as source:
+            print("Listening...")
+            audio = recognizer.listen(source)
+
+            try:
+                # Recognize user voice
+                user_input = recognizer.recognize_google(audio, language=language)
+                print("User: " + user_input)
+
+                # Generate a response
+                user_input_str = str(user_input)
+                _, chat_history = generate_response(user_input_str, conversation_history)
+
+            except sr.UnknownValueError:
+                print("Google Speech Recognition could not understand audio")
+            except sr.RequestError as e:
+                print("Could not request results from Google Speech Recognition service; {0}".format(e))
