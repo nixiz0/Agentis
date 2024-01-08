9b8b96f915c46b0867349cfd3cfb3193a145f54e
julian nothing <julian.57@outlook.fr>
2024-01-08 18:58:46 +0100

    Add stop and save conversation in talk model

--- audio/audio_chat.py
+++ audio/audio_chat.py
@@ -2,6 +2,7 @@ import pyttsx3
 import speech_recognition as sr
 import requests
 import json
+import os
 
 
 def start_talk_chatbot(model, language="en-EN", mic_index=0, voice_id='HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\MSTTS_V110_enGB_HazelM'):
@@ -19,8 +20,7 @@ def start_talk_chatbot(model, language="en-EN", mic_index=0, voice_id='HKEY_LOCA
     recognizer = sr.Recognizer()
 
     def generate_response(prompt, chat_history):
-        conversation_history.append(prompt)
-        full_prompt = "\n".join(map(str, conversation_history))
+        full_prompt = "\n".join(map(str, chat_history))
         
         data = {
             "model": model,
@@ -43,7 +43,14 @@ def start_talk_chatbot(model, language="en-EN", mic_index=0, voice_id='HKEY_LOCA
             return "", chat_history
         else:
             return "Error: Unable to fetch response", chat_history
-    
+
+    def save_conversation(chat_history):
+        download_folder = os.path.expanduser("~\\Downloads")
+        with open(os.path.join(download_folder, 'conversation_with_ai.txt'), 'w') as f:
+            for prompt, response in chat_history:
+                f.write("\nUser: " + prompt + "\n")
+                f.write("\nAssistant: " + response + "\n")
+
     while True:
         with sr.Microphone(device_index=mic_index) as source:
             print("Listening...")
@@ -53,6 +60,22 @@ def start_talk_chatbot(model, language="en-EN", mic_index=0, voice_id='HKEY_LOCA
                 # Recognize user voice
                 user_input = recognizer.recognize_google(audio, language=language)
                 print("User: " + user_input)
+
+                # Check if the user wants to save the conversation
+                detect_save_keyords = ['sauvegarde notre discussion', 'sauvegarde notre conversation', 
+                                       'save our discussion', 'save our conversation']
+                if any(keyword in user_input.lower() for keyword in detect_save_keyords):
+                    save_conversation(conversation_history)
+                    print("Conversation saved.")
+                    continue
+
+                # Check if the user wants to stop the conversation
+                detect_stop_keyords = ['stop notre discussion', 'stop our discussion',
+                                       'stop notre conversation', 'stop our conversation']
+                if any(keyword in user_input.lower() for keyword in detect_stop_keyords):
+                    engine.say('okai bye')
+                    print("Stopping the conversation.")
+                    break
 
                 # Generate a response
                 user_input_str = str(user_input)
