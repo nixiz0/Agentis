c6dbc41f144f27c376d696f6bb649fae5c58e75a
julian nothing <julian.57@outlook.fr>
2024-01-08 22:31:39 +0100

    Add the search youtube command and datetime

--- audio/audio_chat.py
+++ audio/audio_chat.py
@@ -1,10 +1,11 @@
 import csv
-import datetime
 import pyttsx3
 import speech_recognition as sr
+import pywhatkit
 import requests
 import json
 import os
+import datetime
 
 
 def start_talk_chatbot(model, language="en-EN", mic_index=0, voice_id='HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\MSTTS_V110_enGB_HazelM'):
@@ -66,6 +67,31 @@ def start_talk_chatbot(model, language="en-EN", mic_index=0, voice_id='HKEY_LOCA
                 # Recognize user voice
                 user_input = recognizer.recognize_google(audio, language=language)
                 print("User: " + user_input)
+                
+                # Check if the user wants to search in YouTube a video
+                detect_youtube_keywords = ['recherche sur youtube', 'find on youtube', 'find in youtube']
+                if any(keyword in user_input.lower() for keyword in detect_youtube_keywords):
+                    ytb_command = user_input.replace('Open YouTube and find', '')
+                    pywhatkit.playonyt(ytb_command)
+                    continue
+                
+                # Check if the user wants to check the time
+                detect_time_keywords = ['quelle heure est-il', 'l\'heure actuelle', 'what time is it']
+                if any(keyword in user_input.lower() for keyword in detect_time_keywords):
+                    engine.say(datetime.datetime.now().strftime('%H:%M:%S'))
+                    engine.runAndWait()
+                    continue
+                
+                # Check if the user wants to check the date
+                detect_datetime_keywords = ['date actuelle', 'date d\'aujourd\'hui',
+                                            'current date', 'today\'s date', 'date of today'
+                                            ]
+                if any(keyword in user_input.lower() for keyword in detect_datetime_keywords):
+                    current_datetime = datetime.datetime.now()
+                    formatted_datetime = current_datetime.strftime('%A %d %B %Y - %H:%M')
+                    engine.say(formatted_datetime)
+                    engine.runAndWait()
+                    continue
                 
                 # Check if the user wants to save the conversation
                 detect_save_keyords = ['sauvegarde notre discussion', 'sauvegarde notre conversation', 'sauvegarde la discussion', 'sauvegarde la conversation',
